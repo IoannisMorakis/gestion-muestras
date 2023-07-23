4ab136668250a97785dccb1fe0f686beb1bed988
IoannisMorakis <yannimorakis@hotmail.com>
2023-07-22 22:42:07 -0400

    My App

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'gestion-de-muestras-de-campo',
+    appId: '1:956362829056:web:235ab7e9c2cbd05ffe97e5',
+    databaseURL: 'https://gestion-de-muestras-de-campo-default-rtdb.firebaseio.com',
+    storageBucket: 'gestion-de-muestras-de-campo.appspot.com',
+    apiKey: 'AIzaSyDeImqmNn3CZpIAB1A9_q78bnqPVNM1ji8',
+    authDomain: 'gestion-de-muestras-de-campo.firebaseapp.com',
+    messagingSenderId: '956362829056',
+    measurementId: 'G-T5SRLVHLR3',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'gestion-de-muestras-de-campo',
+    appId: '1:956362829056:web:235ab7e9c2cbd05ffe97e5',
+    databaseURL: 'https://gestion-de-muestras-de-campo-default-rtdb.firebaseio.com',
+    storageBucket: 'gestion-de-muestras-de-campo.appspot.com',
+    apiKey: 'AIzaSyDeImqmNn3CZpIAB1A9_q78bnqPVNM1ji8',
+    authDomain: 'gestion-de-muestras-de-campo.firebaseapp.com',
+    messagingSenderId: '956362829056',
+    measurementId: 'G-T5SRLVHLR3',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
