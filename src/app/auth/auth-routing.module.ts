c1fdf26e40d354a76262529d0e2bfe4b17dc2a38
IoannisMorakis <yannimorakis@hotmail.com>
2023-07-23 01:53:02 -0400

    Link Routing

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -7,7 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path:'auth', component: AuthComponent, children:[
+    path:'#/auth', component: AuthComponent, children:[
     {path: 'login', component: LoginComponent},
     {path: 'signup', component: SignupComponent}
   ]}
