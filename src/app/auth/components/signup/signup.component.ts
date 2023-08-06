80770e84c5723adc7c8ada8256e3d2d1721b4025
IoannisMorakis <yannimorakis@hotmail.com>
2023-08-05 23:58:33 -0400

    commit

--- src/app/auth/components/signup/signup.component.ts
+++ src/app/auth/components/signup/signup.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 import {Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from '@angular/fire/auth'
+import {
+  addDoc,
+  setDoc,
+  Firestore,
+  collection,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc
+} from '@angular/fire/firestore'
 
 @Component({
   selector: 'app-signup',
@@ -7,8 +17,10 @@ import {Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from '
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  title = 'gestion-de-muestras-de-campo';
+  public data: any = []
 
-  constructor(public auth: Auth){
+  constructor(public auth: Auth, public firestore: Firestore){
 
   }
 
@@ -19,8 +31,20 @@ export class SignupComponent {
      })
      .catch((err) =>{
       alert(err.message);
-     })
+     });
+     this.addData(value);
+  }
 
+  addData(value: any) {
+    //const dbInstance = collection(this.firestore, 'users');
+    const dbInstance = doc(this.firestore, 'users', value.email);
+    setDoc(dbInstance, value)
+      .then(() => {
+        alert('Data Sent')
+      })
+      .catch((err) => {
+        alert(err.message)
+      })
   }
 
 }
