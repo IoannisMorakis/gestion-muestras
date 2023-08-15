0b04bf4ba5a54e8e89acf1e63737e093847eaa16
IoannisMorakis <yannimorakis@hotmail.com>
2023-08-14 23:54:52 -0400

    projects

---
+++ src/app/user/components/projects/projects.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { Firestore, collection, deleteDoc, doc, getDocs, updateDoc } from '@angular/fire/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css']
+})
+export class ProjectsComponent {
+  public data: any = []
+
+  constructor(private router: Router, private route: ActivatedRoute, public auth: Auth, public firestore: Firestore) {
+    this.getData()
+  }
+
+  getData() {
+    //console.log(this.auth.currentUser)
+    const dbInstance = collection(this.firestore, 'projects');
+    getDocs(dbInstance)
+      .then((response) => {
+        this.data = [...response.docs.map((item) => {
+          return { ...item.data(), id: item.id }
+        })]
+      })
+  }
+
+
+  updateData(id: string) {
+    const dataToUpdate = doc(this.firestore, 'users', id);
+    updateDoc(dataToUpdate, {})
+      .then(() => {
+        alert('Data updated');
+        this.getData()
+      })
+      .catch((err) => {
+        alert(err.message)
+      })
+  }
+
+  deleteData(id: string) {
+    const dataToDelete = doc(this.firestore, 'users', id);
+    deleteDoc(dataToDelete)
+    .then(() => {
+      alert('Data Deleted');
+      this.getData()
+    })
+    .catch((err) => {
+      alert(err.message)
+    })
+  }
+
+}
--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'user', component: UserComponent, children:[
+    {path: 'projects', component: ProjectsComponent}
+  ]}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
